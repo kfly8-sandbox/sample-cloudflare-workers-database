6c65c6095009cdd6b1f1d63c8810cf7d72055991
kobaken <kentafly88@gmail.com>
2025-05-15 23:10:18 +0900

    Added neon with kv cache

--- neon/src/index.ts
+++ neon/src/index.ts
@@ -1,4 +1,19 @@
-import { neon } from '@neondatabase/serverless';
+import { neon, type NeonQueryFunction } from '@neondatabase/serverless';
+
+const sqlWithKV = async (nq: NeonQueryFunction<false,false>, kv: KVNamespace<string>, query: string) => {
+  const cacheKey = `sql:${query}`;
+  const cachedValue = await kv.get(cacheKey);
+  if (cachedValue) {
+    const parsedValue = JSON.parse(cachedValue);
+    return parsedValue;
+  }
+  else {
+    const rows = await nq.query(query);
+    const stringifiedValue = JSON.stringify(rows);
+    await kv.put(cacheKey, stringifiedValue, { expirationTtl: 60 });
+    return rows;
+  }
+}
 
 export default {
   async fetch(request, env, ctx): Promise<Response> {
@@ -14,8 +29,17 @@ export default {
     }
 
     if (
+      request.method === 'GET' &&
+      url.pathname === '/api/todos/with-kv'
+    ) {
+      const kv = env.sample_cloudflare_workers_database_neon_cache
+      const rows = await sqlWithKV(sql, kv, 'SELECT * FROM todos');
+      return Response.json(rows);
+    }
+
+    if (
       request.method === 'POST' &&
-      url.pathname === '/api/todos'
+      url.pathname === '/api/todos/with-cache'
     ) {
       const todo = {
         title: 'Hello World',
